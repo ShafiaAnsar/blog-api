bd40fa6768c6ad00383d1930caaf5bd1e6a6114c
ShafiaAnsar <shafiaansar142@gmail.com>
2023-08-29 18:11:18 +0500

    database connection

--- index.js
+++ index.js
@@ -1,15 +1,22 @@
 const express = require('express');
 const cors = require('cors');
+require('dotenv').config();
 const app = express();
+const connectdb= require("./db")
 
 app.use(cors());
 app.use(express.json());
 
 app.post('/register',(req,res)=>{
+    const {username,email,password} = req.body
     console.log('Received registration request:', req.body);
 
-    res.status(200).json({ message: 'Registration successful' });
+    res.status(200).json({ requestData:{username,email,password} });
 
 })
-
-app.listen(4000)
+connectdb()
+app.listen(4000,()=>{
+    console.log('App is running at port 4000')
+})
+// mooongodb:
+// password:t8jhVJGZKQzAvOzd
