ea42e86e2ee12b42c6ed63150ac88795b5d8c87d
ShafiaAnsar <shafiaansar142@gmail.com>
2023-08-31 18:43:57 +0500

    is logged in

--- index.js
+++ index.js
@@ -6,11 +6,13 @@ const jwt = require("jsonwebtoken")
 const User = require("./models/User")
 const app = express();
 const connectdb= require("./db")
-
+const cookieParser = require('cookie-parser')
 
 const salt = bcrypt.genSaltSync(10)
 const secret ='jf803u4dj02948jkfaalrla'
 app.use(cors({credentials:true,origin:"http://localhost:3000"}));
+
+app.use(cookieParser())
 app.use(express.json());
 
 app.post('/register', async(req,res)=>{
@@ -25,9 +27,11 @@ app.post('/register', async(req,res)=>{
     res.status(200).json(user);
  } catch (error) {
     if (error.code === 11000) {
+      console.log(error)
         // Unique constraint violation
         const duplicateField = Object.keys(error.keyValue)[0];
         res.status(400).json({ error: `This ${duplicateField} is already taken.` });
+        
       } else {
         res.status(500).json({ error: 'An error occurred during registration.' });
       }
@@ -45,7 +49,6 @@ app.post('/login', async (req, res) => {
       res.status(404).json({ error: 'User not found.' });
       return;
     }
-
     const isPasswordValid = bcrypt.compareSync(password, user.password);
     if(isPasswordValid){
       jwt.sign({username,id:user._id},secret,{},(err,token)=>{
@@ -54,19 +57,27 @@ app.post('/login', async (req, res) => {
         }
         res.cookie('token',token).json("ok")
     })
-    // else {
-    //   res.status(401).json({ error: 'Invalid password.' })
-    //   });
-    //   return;
-    // }
   } 
+  else {
+    res.status(401).json({ error: 'Invalid password.' })
+    }
+  
 }
   catch (error) {
     console.error('An error occurred during login:', error);
     res.status(500).json({ error: 'An error occurred during login.' });
   }
 });
-
+app.get('/profile', (req,res) => {
+  const {token} = req.cookies;
+  jwt.verify(token, secret, {}, (err,info) => {
+    if (err) throw err;
+    res.json(info);
+  });
+});
+app.post('/logout', (req,res) => {
+  res.cookie('token', '').json('ok');
+});
 connectdb()
 app.listen(4000,()=>{
     console.log('App is running at port 4000')
