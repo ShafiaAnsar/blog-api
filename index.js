c03f1136812509e91d04de9657ab6e4687598140
ShafiaAnsar <shafiaansar142@gmail.com>
2023-08-31 14:30:30 +0500

    Loggin in

--- index.js
+++ index.js
@@ -2,13 +2,15 @@ const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
 const bcrypt =require('bcryptjs')
+const jwt = require("jsonwebtoken")
 const User = require("./models/User")
 const app = express();
 const connectdb= require("./db")
 
 
 const salt = bcrypt.genSaltSync(10)
-app.use(cors());
+const secret ='jf803u4dj02948jkfaalrla'
+app.use(cors({credentials:true,origin:"http://localhost:3000"}));
 app.use(express.json());
 
 app.post('/register', async(req,res)=>{
@@ -32,11 +34,39 @@ app.post('/register', async(req,res)=>{
     }
 
 })
-app.post('/login',async(req,res)=>{
-  const {username,password} = req.body
-    console.log('Received registration request:', req.body)
-    const user = User.findOne({username})
-})
+app.post('/login', async (req, res) => {
+  const { username, password } = req.body;
+  console.log('Received login request:', req.body);
+
+  try {
+    const user = await User.findOne({ username });
+
+    if (!user) {
+      res.status(404).json({ error: 'User not found.' });
+      return;
+    }
+
+    const isPasswordValid = bcrypt.compareSync(password, user.password);
+    if(isPasswordValid){
+      jwt.sign({username,id:user._id},secret,{},(err,token)=>{
+        if(err){
+          throw new Error(err)
+        }
+        res.cookie('token',token).json("ok")
+    })
+    // else {
+    //   res.status(401).json({ error: 'Invalid password.' })
+    //   });
+    //   return;
+    // }
+  } 
+}
+  catch (error) {
+    console.error('An error occurred during login:', error);
+    res.status(500).json({ error: 'An error occurred during login.' });
+  }
+});
+
 connectdb()
 app.listen(4000,()=>{
     console.log('App is running at port 4000')
